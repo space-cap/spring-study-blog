2d644d0fb82a62cfa4e693167af0e80930f91e01
COCO <hong.git@yahoo.com>
2025-05-21 18:23:15 +0900

    주사위 개수 받기

--- workspace430/ch2/src/main/java/com/fastcampus/ch2/DiceController.java
+++ workspace430/ch2/src/main/java/com/fastcampus/ch2/DiceController.java
@@ -2,31 +2,42 @@ package com.fastcampus.ch2;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
+import java.util.ArrayList;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
 @Controller
 public class DiceController {
-	
+	public static final int DICE_SIDES = 6;
+	public static final int MAX_NUM_OF_DICE = 4;
+	public static final int MIN_NUM_OF_DICE = 2;
+
 	@RequestMapping(value = "/dice", method = RequestMethod.GET)
-	public void dice(HttpServletResponse response) throws IOException {
+	public void dice(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+		int diceCount = MIN_NUM_OF_DICE;
+		if (request.getParameter("dc") != null) {
+			diceCount = Integer.parseInt(request.getParameter("dc"));
+			if (diceCount < MIN_NUM_OF_DICE || diceCount > MAX_NUM_OF_DICE) {
+				diceCount = MIN_NUM_OF_DICE;
+			}
+		}
+		System.out.println("diceCount=" + diceCount);
+
 		response.setContentType("text/html; charset=UTF-8");
-	    response.setCharacterEncoding("UTF-8");
-	    
-		int idx1 = (int)(Math.random() * 6) + 1;
-		int idx2 = (int)(Math.random() * 6) + 1;
-		
+		response.setCharacterEncoding("UTF-8");
+
+		ArrayList<Integer> diceResults = new ArrayList<>();
+		for (int i = 0; i < diceCount; i++) {
+			int dice = (int) (Math.random() * DICE_SIDES) + 1;
+			diceResults.add(dice);
+		}
+
 		PrintWriter out = response.getWriter();
 		out.println("<html>");
 		out.println("<head>");
@@ -35,10 +46,12 @@ public class DiceController {
 		out.println("</head>");
 		out.println("<body>");
 		out.println("<h1>주사위</h1>");
-		out.println("<img src='resources/dice/dice"+ idx1 +".jpg'>");
-		out.println("<img src='resources/dice/dice"+ idx2 +".jpg'>");
+		for (var result : diceResults) {
+			int dice = result;
+			out.println("<img src='resources/dice/dice" + dice + ".jpg'>");
+		}
 		out.println("</body>");
 		out.println("</html>");
 	}
-	
+
 }
