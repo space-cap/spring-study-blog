090b7ac5e98db46c948b08a3034abc6d76acc0af
COCO <hong.git@yahoo.com>
2025-05-21 18:49:25 +0900

    심플하게 수정.

--- workspace430/ch2/src/main/java/com/fastcampus/ch2/Dice2Controller.java
+++ workspace430/ch2/src/main/java/com/fastcampus/ch2/Dice2Controller.java
@@ -1,16 +1,7 @@
 package com.fastcampus.ch2;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.text.DateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.Locale;
 
-import javax.servlet.http.HttpServletResponse;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,20 +10,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class Dice2Controller {
 	public static final int DICE_SIDES = 6;
-	public static final int NUM_OF_DICE = 2;
 
 	@RequestMapping(value = "/dice2", method = RequestMethod.GET)
 	public String dice2(Model model) throws IOException {
 
-		int diceCount = NUM_OF_DICE;
+		int dice1 = (int) (Math.random() * DICE_SIDES) + 1;
+		int dice2 = (int) (Math.random() * DICE_SIDES) + 1;
 
-		ArrayList<Integer> diceResults = new ArrayList<>();
-		for (int i = 0; i < diceCount; i++) {
-			int dice = (int) (Math.random() * DICE_SIDES) + 1;
-			diceResults.add(dice);
-		}
-
-		model.addAttribute("diceResults", diceResults);
+		model.addAttribute("dice1", dice1);
+		model.addAttribute("dice2", dice2);
 
 		return "dice2";
 	}
--- workspace430/ch2/src/main/webapp/WEB-INF/views/dice2.jsp
+++ workspace430/ch2/src/main/webapp/WEB-INF/views/dice2.jsp
@@ -1,18 +1,14 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ page session="false" %>
 <%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
-
-
 <html>
 <head>
 	<title>Dice 2</title>
 </head>
 <body>
 <h1>
-	<c:forEach var="dice" items="${diceResults}" varStatus="status">
-	        주사위 ${status.index + 1} : ${dice} <br>
-	    </c:forEach> 
+	주사위 1 : ${dice1} <br>
+	주사위 2 : ${dice2} <br>  
 </h1>
-
 </body>
 </html>
