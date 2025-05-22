f4da0ebc4d2aa306f7cae7b6860decca0c7a3eea
COCO <hong.git@yahoo.com>
2025-05-22 19:00:21 +0900

    Update MiniSpringContext.java

--- workspace430/ch3/src/main/java/com/fastcampus/ch3/diCopy1/MiniSpringContext.java
+++ workspace430/ch3/src/main/java/com/fastcampus/ch3/diCopy1/MiniSpringContext.java
@@ -19,15 +19,24 @@ public class MiniSpringContext {
 	public static void main(String[] args) throws Exception {
 		System.out.println("Hello, Mini Spring!");
 
-		Car car = getCar();
-		System.out.println("car = " + car);
+		Car car1 = getCar();
+		Car car2 = getCar();
+		Car car3 = getCar();
+		System.out.println("car1 = " + car1);
+		System.out.println("car2 = " + car2);
+		System.out.println("car3 = " + car3);
 	}
 
+	public static void registerBean() throws Exception{
+		
+	}
+	
 	public static Car getCar() throws Exception {
+
 		// config.txt를 읽어서 Properties에 저장
 		Properties p = new Properties();
 		p.load(new FileReader("src/main/resources/config.txt"));
-
+				
 	    // 클래스 객체(설계도)를 얻어서
 	    Class<?> clazz = Class.forName(p.getProperty("car"));
 	    return (Car) clazz.getDeclaredConstructor().newInstance();  // 객체를 생성해서 반환
