a3f68064f533a6f0f066734dca4a9203b23ed16e
COCO <hong.git@yahoo.com>
2025-05-22 19:56:31 +0900

    google guava  추가.

--- workspace430/ch3/src/main/java/com/fastcampus/ch3/diCopy3/Main3.java
+++ workspace430/ch3/src/main/java/com/fastcampus/ch3/diCopy3/Main3.java
@@ -1,5 +1,75 @@
 package com.fastcampus.ch3.diCopy3;
 
-public class Main3 {
+import com.google.common.reflect.ClassPath;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+@Component
+class Car {
+}
+
+@Component
+class SportsCar extends Car {
+}
+
+@Component
+class Truck extends Car {
+}
+
+@Component
+class Engine {
+}
+
+class AppContext {
+	private Map map;
+	
+	AppContext() {
+		map = new HashMap();
+        doComponentScan();
+	}
+	
+	private void doComponentScan() {
+		try {
+            // 1. 패키지내의 클래스 목록을 가져온다.
+            ClassLoader classLoader = AppContext.class.getClassLoader();
+            ClassPath classPath = ClassPath.from(classLoader);
+
+            Set<ClassPath.ClassInfo> set = classPath.getTopLevelClasses("com.fastcampus.ch3.diCopy3");
+
+            // 2. 반복문으로 클래스를 하나씩 읽어와서 @Component 애너테이션이 붙어있는지 확인
+            for (ClassPath.ClassInfo classInfo : set) {
+                Class clazz = classInfo.load();
+                Component component = (Component) clazz.getAnnotation(Component.class);
+                // 3. @Componet 애너테이션이 붙어있으면 객체를 생성해서 Map에 저장
+                if (component != null) {
+                    String id = StringUtils.uncapitalize(classInfo.getSimpleName());
+                    map.put(id, clazz.getDeclaredConstructor().newInstance());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+	}
+	
+	public Object getBean(String key) {
+		return map.get(key);
+	}
+}
+
+public class Main3 {
+	public static void main(String[] args) {
+		System.out.println("Hello, Main3!");
+		
+		AppContext ac = new AppContext();
+		
+		Car car = (Car) ac.getBean("car");
+        Engine engine = (Engine) ac.getBean("engine");
+        System.out.println("car = " + car);
+        System.out.println("engine = " + engine);
+	}
 }
