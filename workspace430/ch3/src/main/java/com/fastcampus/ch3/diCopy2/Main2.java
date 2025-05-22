8b5d65e1c9f42edeb719aa33d06d0ab3365b9475
COCO <hong.git@yahoo.com>
2025-05-22 19:45:30 +0900

    main3

--- workspace430/ch3/src/main/java/com/fastcampus/ch3/diCopy2/Main2.java
+++ workspace430/ch3/src/main/java/com/fastcampus/ch3/diCopy2/Main2.java
@@ -1,7 +1,9 @@
 package com.fastcampus.ch3.diCopy2;
 
+import java.io.FileReader;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 class Car {
 }
@@ -18,15 +20,32 @@ class Engine {
 class AppContext {
 	Map map; // 객체 저장소
 
-	AppContext() {
-		map = new HashMap();
-		map.put("car", new SportsCar());
-		map.put("engine", new Engine());
-	}
+    AppContext() {
 
-	Object getBean(String key) {
-		return map.get(key);
-	}
+        try {
+            Properties p = new Properties();
+            p.load(new FileReader("src/main/resources/config.txt"));
+
+            // Properties에 저장된 내용을 Map에 저장
+            map = new HashMap(p); // 지정된 Map의 모든 요소를 포함하는 HashMap을 생성.
+
+            // 반복문으로 클래스 이름을 얻어서 객체를 생성해서 다시 map에 저장
+            for (Object key : map.keySet()) {
+                // Map에 저장된 key를 이용해서 class 정보를 가져와야 한다.
+            	Class<?> clazz = Class.forName((String) map.get(key));
+
+                // 객체를 만들어서 Map에 저장
+                map.put(key, clazz.getDeclaredConstructor().newInstance());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    Object getBean(String key) {
+        return map.get(key);
+    }
 }
 
 public class Main2 {
---
+++ workspace430/ch3/src/main/java/com/fastcampus/ch3/diCopy3/Main3.java
@@ -0,0 +1,5 @@
+package com.fastcampus.ch3.diCopy3;
+
+public class Main3 {
+
+}
