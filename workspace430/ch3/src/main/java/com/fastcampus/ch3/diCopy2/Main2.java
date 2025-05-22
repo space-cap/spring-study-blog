a9cc75a166e9f2b86f0779f432967c85c81b81ff
COCO <hong.git@yahoo.com>
2025-05-22 19:28:41 +0900

    Update Main2.java

--- workspace430/ch3/src/main/java/com/fastcampus/ch3/diCopy2/Main2.java
+++ workspace430/ch3/src/main/java/com/fastcampus/ch3/diCopy2/Main2.java
@@ -3,37 +3,43 @@ package com.fastcampus.ch3.diCopy2;
 import java.util.HashMap;
 import java.util.Map;
 
-class Car {}
-class SportsCar extends Car{}
-class Truck extends Car {}
-class Engine {}
-
-class AppContext {
-    Map map; // 객체 저장소
-
-    AppContext() {
-        map = new HashMap();
-        map.put("car", new SportsCar());
-        map.put("engine", new Engine());
-    }
-
-    Object getBean(String key) {
-        return map.get(key);
-    }
+class Car {
 }
 
+class SportsCar extends Car {
+}
+
+class Truck extends Car {
+}
+
+class Engine {
+}
+
+class AppContext {
+	Map map; // 객체 저장소
+
+	AppContext() {
+		map = new HashMap();
+		map.put("car", new SportsCar());
+		map.put("engine", new Engine());
+	}
+
+	Object getBean(String key) {
+		return map.get(key);
+	}
+}
 
 public class Main2 {
 	public static void main(String[] args) {
 		System.out.println("Hello, Main2!");
-		
+
 		AppContext ac = new AppContext();
-		
+
 		Car car = (Car) ac.getBean("car");
-        Engine engine = (Engine) ac.getBean("engine");
-        System.out.println("car = " + car);
-        System.out.println("engine = " + engine);
-        
+		Engine engine = (Engine) ac.getBean("engine");
+		System.out.println("car = " + car);
+		System.out.println("engine = " + engine);
+
 	}
 
 }
