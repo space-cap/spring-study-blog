caf704d6d765230c10545032979eca0f450d4fe8
COCO <hong.git@yahoo.com>
2025-05-22 19:19:23 +0900

    main2

--- workspace430/ch3/src/main/java/com/fastcampus/ch3/diCopy1/MiniSpringContext.java
+++ workspace430/ch3/src/main/java/com/fastcampus/ch3/diCopy1/MiniSpringContext.java
@@ -19,16 +19,28 @@ public class MiniSpringContext {
 	public static void main(String[] args) throws Exception {
 		System.out.println("Hello, Mini Spring!");
 
-		Car car1 = getCar();
-		Car car2 = getCar();
-		Car car3 = getCar();
-		System.out.println("car1 = " + car1);
-		System.out.println("car2 = " + car2);
-		System.out.println("car3 = " + car3);
+//		Car car1 = getCar();
+//		Car car2 = getCar();
+//		Car car3 = getCar();
+//		System.out.println("car1 = " + car1);
+//		System.out.println("car2 = " + car2);
+//		System.out.println("car3 = " + car3);
+		
+		Car car = (Car) getObject("car");
+	    Engine engine = (Engine) getObject("engine");
+	    System.out.println("car = " + car);
+	    System.out.println("engine = " + engine);
+		
 	}
 
-	public static void registerBean() throws Exception{
-		
+	static Object getObject(String key) throws Exception {
+	    // config.txt를 읽어서 Properties에 저장
+	    Properties p = new Properties();
+	    p.load(new FileReader("src/main/resources/config.txt"));
+
+	    // 클래스 객체(설계도)를 얻어서
+	    Class<?> clazz = Class.forName(p.getProperty(key));
+	    return clazz.getDeclaredConstructor().newInstance();
 	}
 	
 	public static Car getCar() throws Exception {
---
+++ workspace430/ch3/src/main/java/com/fastcampus/ch3/diCopy2/Main2.java
@@ -0,0 +1,8 @@
+package com.fastcampus.ch3.diCopy2;
+
+public class Main2 {
+	public static void main(String[] args) {
+		System.out.println("Hello, Main2!");
+	}
+
+}
