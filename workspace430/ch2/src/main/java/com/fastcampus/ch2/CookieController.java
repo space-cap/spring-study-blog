5ba337c54e66622a810740456fa7c532ea3ff931
COCO <hong.git@yahoo.com>
2025-05-23 14:13:48 +0900

    session

--- workspace430/ch2/src/main/java/com/fastcampus/ch2/CookieController.java
+++ workspace430/ch2/src/main/java/com/fastcampus/ch2/CookieController.java
@@ -4,6 +4,11 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -12,10 +17,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class HelloController {
-	private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
-	@RequestMapping(value = "/hello", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+public class CookieController {
+	private static final Logger logger = LoggerFactory.getLogger(CookieController.class);
+	@RequestMapping(value = "/coo", method = RequestMethod.GET)
+	public String home(Locale locale, HttpServletRequest request, HttpServletResponse response, Model model) {
 		logger.info("hello! The client locale is {}.", locale);
 		
 		Date date = new Date();
@@ -25,6 +30,14 @@ public class HelloController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		model.addAttribute("name", "영현");
+		
+		Cookie cookie = new Cookie("name", "king");
+		cookie.setPath("/"); // 쿠키가 모든 경로에서 유효
+		cookie.setMaxAge(30 * 60); // 30분
+		response.addCookie(cookie);
+
+		HttpSession session = request.getSession();
+	    session.setAttribute("userId", "steve");
 		
 		return "hello";
 	}
