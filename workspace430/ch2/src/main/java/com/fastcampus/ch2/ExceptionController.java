98b8c98649635180843e694a8bce72db43934b19
COCO <hong.git@yahoo.com>
2025-05-25 01:22:23 +0900

    h

--- workspace430/ch2/src/main/java/com/fastcampus/ch2/ExceptionController.java
+++ workspace430/ch2/src/main/java/com/fastcampus/ch2/ExceptionController.java
@@ -1,5 +1,7 @@
 package com.fastcampus.ch2;
 
+import java.io.FileNotFoundException;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,6 +10,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ExceptionController {
 	
+//	@ExceptionHandler(Exception.class)
+//	public String catcher(Exception e, Model model) {
+//		System.out.println("ExceptionController.catcher() called");
+//		e.printStackTrace();
+//		model.addAttribute("ex", e);
+//		return "error";
+//	}
+	
 	@RequestMapping("/ex")
 	public String main() throws Exception {
 		throw new Exception("예외가 발생했습니다. zzz");
@@ -15,6 +25,7 @@ public class ExceptionController {
 	
 	@RequestMapping("/ex2")
 	public String main2() throws Exception {
-		throw new Exception("예외가 발생했습니다. zzz");
+		//throw new Exception("예외가 발생했습니다. zzz");
+		throw new FileNotFoundException("파일을 찾을 수 없습니다. zzz");
 	}
 }
--- workspace430/ch2/src/main/java/com/fastcampus/ch2/GlobalCatcher.java
+++ workspace430/ch2/src/main/java/com/fastcampus/ch2/GlobalCatcher.java
@@ -5,19 +5,28 @@ import java.io.FileNotFoundException;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice // 모든 @Controller에 적용되는 ExceptionHandler
 public class GlobalCatcher {
-	@ExceptionHandler({NullPointerException.class, FileNotFoundException.class})
+	@ExceptionHandler({NullPointerException.class})
 	public String catcher2(Exception ex, Model m) {
+		System.out.println("GlobalCatcher.catcher2() called");
 		m.addAttribute("ex", ex);
 		return "error";
 	}
 
 	@ExceptionHandler(Exception.class)
 	public String catcher(Exception ex, Model m) {
+		System.out.println("GlobalCatcher.catcher() called");
 		m.addAttribute("ex", ex);
-
 		return "error";
 	}
+	
+	@ExceptionHandler(FileNotFoundException.class)
+    public ModelAndView handleFileNotFoundException(FileNotFoundException ex) {
+        ModelAndView mav = new ModelAndView("error");
+        mav.addObject("message", ex.getMessage());
+        return mav;
+    }
 }
