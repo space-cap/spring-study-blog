43d3b282a67b503892a2a9610e7caa6d65290396
COCO <hong.git@yahoo.com>
2025-05-25 00:55:37 +0900

    Update ExceptionController.java

--- workspace430/ch2/src/main/java/com/fastcampus/ch2/ExceptionController.java
+++ workspace430/ch2/src/main/java/com/fastcampus/ch2/ExceptionController.java
@@ -15,4 +15,15 @@ public class ExceptionController {
 			return "error";
 		}
 	}
+	
+	@RequestMapping("/ex2")
+	public String main2() throws Exception {
+		try {
+			throw new Exception("예외가 발생했습니다. zzz");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			//e.printStackTrace();
+			return "error";
+		}
+	}
 }
