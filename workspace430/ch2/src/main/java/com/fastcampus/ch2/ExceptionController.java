543b55245d7137b2b35e92eeee5a5a3ed8850c57
COCO <hong.git@yahoo.com>
2025-05-25 01:04:43 +0900

    aa

--- workspace430/ch2/src/main/java/com/fastcampus/ch2/ExceptionController.java
+++ workspace430/ch2/src/main/java/com/fastcampus/ch2/ExceptionController.java
@@ -1,29 +1,28 @@
 package com.fastcampus.ch2;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ExceptionController {
+	
+	@ExceptionHandler(Exception.class)
+	public String catcher(Exception e, Model model) {
+		// TODO Auto-generated method stub
+		e.printStackTrace();
+		model.addAttribute("ex", e);
+		return "error";
+	}
+	
 	@RequestMapping("/ex")
 	public String main() throws Exception {
-		try {
-			throw new Exception("예외가 발생했습니다. zzz");
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			//e.printStackTrace();
-			return "error";
-		}
+		throw new Exception("예외가 발생했습니다. zzz");
 	}
 	
 	@RequestMapping("/ex2")
 	public String main2() throws Exception {
-		try {
-			throw new Exception("예외가 발생했습니다. zzz");
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			//e.printStackTrace();
-			return "error";
-		}
+		throw new Exception("예외가 발생했습니다. zzz");
 	}
 }
