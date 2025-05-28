71b42476d5ac10a43900de9d05efa92a69081ced
COCO <hong.git@yahoo.com>
2025-05-29 02:21:21 +0900

    Update DBConnectionTest2Test.java

--- intellij_work/ch3/src/test/java/com/fastcampus/ch3/DBConnectionTest2Test.java
+++ intellij_work/ch3/src/test/java/com/fastcampus/ch3/DBConnectionTest2Test.java
@@ -1,16 +1,25 @@
 package com.fastcampus.ch3;
 
-
-
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.sql.*;
 import java.sql.*;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "file:src/main/webapp/WEB-INF/spring/**/root-context.xml")
 public class DBConnectionTest2Test {
+
+    @Autowired
+    DataSource ds;
+
     @Test
     public void testSomething() {
         assertEquals(2, 1 + 1);
@@ -20,11 +29,14 @@ public class DBConnectionTest2Test {
     public void testDBConnection() throws Exception {
         System.out.println("DBConnectionTest2Test");
 
-        ApplicationContext ac = new GenericXmlApplicationContext("file:src/main/webapp/WEB-INF/spring/**/root-context.xml");
-        DataSource ds = ac.getBean(DataSource.class);
+//        ApplicationContext ac = new GenericXmlApplicationContext("file:src/main/webapp/WEB-INF/spring/**/root-context.xml");
+//        DataSource ds = ac.getBean(DataSource.class);
 
         Connection conn = ds.getConnection(); // 데이터베이스의 연결을 얻는다.
 
         System.out.println("conn = " + conn);
+
+        //assertTrue(conn != null);
+        assertNotNull(conn);
     }
 }
