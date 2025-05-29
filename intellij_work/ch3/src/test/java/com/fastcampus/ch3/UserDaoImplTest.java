7d022fe0c7fa00067e3bf2d89213634a13a632b9
COCO <hong.git@yahoo.com>
2025-05-29 16:09:46 +0900

    aa

---
+++ intellij_work/ch3/src/test/java/com/fastcampus/ch3/UserDaoImplTest.java
@@ -0,0 +1,24 @@
+package com.fastcampus.ch3;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDaoImplTest {
+
+    @Test
+    void deleteUser() {
+    }
+
+    @Test
+    void selectUser() {
+    }
+
+    @Test
+    void insertUser() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+}
