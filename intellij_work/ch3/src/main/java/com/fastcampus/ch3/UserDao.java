c4ca17ecd7152f74eec46c9e0f8761acf639b68c
COCO <hong.git@yahoo.com>
2025-05-29 17:26:39 +0900

    aa

--- intellij_work/ch3/src/main/java/com/fastcampus/ch3/UserDao.java
+++ intellij_work/ch3/src/main/java/com/fastcampus/ch3/UserDao.java
@@ -3,7 +3,7 @@ package com.fastcampus.ch3;
 public interface UserDao {
     int deleteUser(String id);
 
-    User selectUser(String id) throws Exception;
+    User selectUser(String id);
 
     // 사용자 정보를 user_info테이블에 저장하는 메서드
     int insertUser(User user);
--- intellij_work/ch3/src/test/java/com/fastcampus/ch3/UserDaoImplTest.java
+++ intellij_work/ch3/src/test/java/com/fastcampus/ch3/UserDaoImplTest.java
@@ -1,13 +1,26 @@
 package com.fastcampus.ch3;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Calendar;
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(SpringExtension.class) // ac 를 만드는 것 하고 같다. // ac 만들어 놓고 계속 사용한다.
+@ContextConfiguration(locations = "file:src/main/webapp/WEB-INF/spring/**/root-context.xml")
 class UserDaoImplTest {
 
+    @Autowired
+    UserDao userDao;
+
     @Test
-    void deleteUser() {
+    void deleteUser(String id) {
+
     }
 
     @Test
@@ -19,6 +32,25 @@ class UserDaoImplTest {
     }
 
     @Test
-    void updateUser() {
+    void updateUser()  {
+
+        Calendar cal = Calendar.getInstance();
+        cal.clear();
+        cal.set(2020, 1, 1, 0, 0, 0);
+
+        User user = new User("asdf22", "1234", "smith", "a@a.com", new Date(cal.getTimeInMillis()), "facebook", new Date());
+        userDao.deleteUser(user.getId());
+        int rowCnt = userDao.insertUser(user);
+
+        assertEquals(1, rowCnt);
+
+        user.setPwd("4545");
+        user.setEmail("b@b.com");
+        rowCnt = userDao.updateUser(user);
+        assertEquals(rowCnt, 1);
+
+        User user2 = userDao.selectUser(user.getId());
+        assertEquals(user.getPwd(), user2.getPwd());
+        assertEquals(user.getEmail(), user2.getEmail());
     }
 }
