519ad3fad6c5e56a0ec5e30ac900355c4841f155
COCO <hong.git@yahoo.com>
2025-06-03 17:58:32 +0900

    aa

---
+++ intellij_work/ch3/src/main/java/com/fastcampus/ch3/aop/AopMain2.java
@@ -0,0 +1,7 @@
+package com.fastcampus.ch3.aop;
+
+public class AopMain2 {
+    public static void main(String[] args) {
+        System.out.println("AopMain2");
+    }
+}
---
+++ intellij_work/ch3/src/main/java/com/fastcampus/ch3/aop/MyMath.java
@@ -0,0 +1,16 @@
+package com.fastcampus.ch3.aop;
+
+public class MyMath {
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public int add(int a, int b, int c) {
+        return a + b + c;
+    }
+    public int sub(int a, int b) {
+        return a - b;
+    }
+    public int mul(int a, int b) {
+        return a * b;
+    }
+}
