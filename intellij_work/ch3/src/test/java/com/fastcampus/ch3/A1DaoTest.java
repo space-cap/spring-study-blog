70175623f2c7f5b7866d64aed0a23506cd5f6266
COCO <hong.git@yahoo.com>
2025-06-04 19:15:34 +0900

    aa

--- intellij_work/ch3/src/test/java/com/fastcampus/ch3/A1DaoTest.java
+++ intellij_work/ch3/src/test/java/com/fastcampus/ch3/A1DaoTest.java
@@ -1,10 +1,17 @@
 package com.fastcampus.ch3;
 
+import javax.sql.DataSource;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,14 +22,32 @@ class A1DaoTest {
     @Autowired
     A1Dao dao;
 
+    @Autowired
+    DataSource ds;
+
     @Test
     public void insertA1() throws Exception {
-        int a = dao.insertA1(3, 10);
-        int b = dao.insertA1(4, 20);
+
+        // 트랜젝션 생성 Tx 매니저 생성
+        PlatformTransactionManager tm = new DataSourceTransactionManager(ds);
+        TransactionStatus status = tm.getTransaction(new DefaultTransactionDefinition());
+
+        // Tx 시작
+        int a = 0;
+        int b = 0;
+        try {
+            a = dao.insertA1(1, 10);
+            b = dao.insertA1(1, 20);
+
+            tm.commit(status);
+        } catch (Exception e) {
+            e.printStackTrace();
+            tm.rollback(status);
+        } finally {
+        }
 
         System.out.println("a="+a+", b="+b);
-
-        assertEquals(1,1);
+        assertEquals(a, b);
     }
 
 }
