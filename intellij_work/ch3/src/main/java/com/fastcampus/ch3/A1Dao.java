44f0995dbb4499386b4724dbd81c7f04ecbe3874
COCO <hong.git@yahoo.com>
2025-06-04 18:38:20 +0900

    aa

--- intellij_work/ch3/src/main/java/com/fastcampus/ch3/A1Dao.java
+++ intellij_work/ch3/src/main/java/com/fastcampus/ch3/A1Dao.java
@@ -14,6 +14,28 @@ public class A1Dao {
     @Autowired
     DataSource ds;
 
+    public int insertA1(int key, int value) throws Exception {
+        Connection conn = null;
+        PreparedStatement ps = null;
+
+        try {
+            conn = ds.getConnection();
+            String sql = "insert into a1 values(?,?)";
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, key);
+            ps.setInt(2, value);
+
+            return ps.executeUpdate();
+        } catch (SQLException e) {
+            //throw new RuntimeException(e);
+            e.printStackTrace();
+        } finally {
+            close(conn, ps);
+        }
+
+        return 0;
+    }
+
     private void close(AutoCloseable... acs) {
         for(AutoCloseable ac :acs)
             try { if(ac!=null) ac.close(); } catch(Exception e) { e.printStackTrace(); }
--- intellij_work/ch3/src/test/java/com/fastcampus/ch3/A1DaoTest.java
+++ intellij_work/ch3/src/test/java/com/fastcampus/ch3/A1DaoTest.java
@@ -8,7 +8,17 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(SpringExtension.class) // ac 를 만드는 것 하고 같다. // ac 만들어 놓고 계속 사용한다.
+@ContextConfiguration(locations = "file:src/main/webapp/WEB-INF/spring/**/root-context.xml")
 class A1DaoTest {
 
+    @Test
+    public void insertA1() throws Exception {
+
+        @Autowired
+        A1Dao dao;
+
+        dao.insertA1(1, 10);
+    }
 
 }
