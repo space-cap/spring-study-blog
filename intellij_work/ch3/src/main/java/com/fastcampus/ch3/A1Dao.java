bdd84b8ac44dd3b5873380d7ab0197d339e0104a
COCO <hong.git@yahoo.com>
2025-06-04 18:36:10 +0900

    aa

--- intellij_work/ch3/src/main/java/com/fastcampus/ch3/A1Dao.java
+++ intellij_work/ch3/src/main/java/com/fastcampus/ch3/A1Dao.java
@@ -1,9 +1,9 @@
 package com.fastcampus.ch3;
 
-import org.apache.tomcat.jdbc.pool.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -13,28 +13,6 @@ public class A1Dao {
 
     @Autowired
     DataSource ds;
-
-    public int insertA1(int key, int value) throws Exception {
-        Connection conn = null;
-        PreparedStatement ps = null;
-
-        try {
-            conn = ds.getConnection();
-            String sql = "insert into a1 values(?,?)";
-            ps = conn.prepareStatement(sql);
-            ps.setInt(1, key);
-            ps.setInt(2, value);
-
-            return ps.executeUpdate();
-        } catch (SQLException e) {
-            //throw new RuntimeException(e);
-            e.printStackTrace();
-        } finally {
-            close(conn, ps);
-        }
-
-        return 0;
-    }
 
     private void close(AutoCloseable... acs) {
         for(AutoCloseable ac :acs)
--- intellij_work/ch3/src/test/java/com/fastcampus/ch3/A1DaoTest.java
+++ intellij_work/ch3/src/test/java/com/fastcampus/ch3/A1DaoTest.java
@@ -8,16 +8,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@ExtendWith(SpringExtension.class) // ac 를 만드는 것 하고 같다. // ac 만들어 놓고 계속 사용한다.
-@ContextConfiguration(locations = "file:src/main/webapp/WEB-INF/spring/**/root-context.xml")
 class A1DaoTest {
 
-    @Autowired
-    A1Dao dao;
 
-    @Test
-    public void insertTest() throws Exception {
-        dao.insertA1(1,200);
-        dao.insertA1(2,200);
-    }
 }
