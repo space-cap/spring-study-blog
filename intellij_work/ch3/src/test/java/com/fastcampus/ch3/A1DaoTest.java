76c0e8a0d8781a07462c2b1f924d210d2831f833
COCO <hong.git@yahoo.com>
2025-06-04 17:49:30 +0900

    aa

--- intellij_work/ch3/src/test/java/com/fastcampus/ch3/A1DaoTest.java
+++ intellij_work/ch3/src/test/java/com/fastcampus/ch3/A1DaoTest.java
@@ -1,7 +1,23 @@
 package com.fastcampus.ch3;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(SpringExtension.class) // ac 를 만드는 것 하고 같다. // ac 만들어 놓고 계속 사용한다.
+@ContextConfiguration(locations = "file:src/main/webapp/WEB-INF/spring/**/root-context.xml")
 class A1DaoTest {
 
+    @Autowired
+    A1Dao dao;
+
+    @Test
+    public void insertTest() throws Exception {
+        dao.insertA1(1,200);
+        dao.insertA1(2,200);
+    }
 }
