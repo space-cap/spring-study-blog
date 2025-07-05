abe03629c02db6d6ffc31959c81f44626bf5401c
이영현 <hong.git@yahoo.com>
2025-07-06 08:45:22 +0900

    Update Main.java

--- boot/ch3/src/main/java/com/fastcampus/ch3/di1/Main.java
+++ boot/ch3/src/main/java/com/fastcampus/ch3/di1/Main.java
@@ -1,23 +1,28 @@
 package com.fastcampus.ch3.di1;
 
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.Properties;
 
+class Car {}
+class SportCar extends Car {}
+class Truck extends Car {}
+class Engin {}
+class Door {}
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("Hello World!");
+
+        Car car = (Car)getObject("car");
     }
 
-    static Object getObject(String key) {
+    static Object getObject(String key) throws Exception {
         Properties prop = new Properties();
         Class clazz = null;
-        try {
-            prop.load(new FileReader("config.txt"));
-            String className = prop.getProperty(key);
-            clazz = Class.forName(className);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        prop.load(new FileReader("config.txt"));
+        String className = prop.getProperty(key);
+        clazz = Class.forName(className);
 
         return clazz.newInstance();
     }
