bbf6b95a8f9d4389aa07603ebdefdee2cd07a7b8
이영현 <hong.git@yahoo.com>
2025-07-06 11:00:44 +0900

    dd

--- boot/ch3/src/main/java/com/fastcampus/ch3/di3/AppConfig.java
+++ boot/ch3/src/main/java/com/fastcampus/ch3/di3/AppConfig.java
@@ -1,23 +1,25 @@
 package com.fastcampus.ch3.di3;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
-//    @Bean
-////    @Scope("singleton")
-//    Car car() {
-//        return new Car();
-//    }
-//    @Bean
-//    @Scope("prototype")
-//    Engine engine() {
-//        return new Engine();
-//    }
-//
-//    @Bean
-////    @Scope("singleton")
-//    Door door() {
-//        return new Door();
-//    }
+    @Bean
+//    @Scope("singleton")
+    Car car() {
+        return new Car();
+    }
+    @Bean
+    @Scope("prototype")
+    Engine engine() {
+        return new Engine();
+    }
+
+    @Bean
+//    @Scope("singleton")
+    Door door() {
+        return new Door();
+    }
 }
--- boot/ch3/src/main/java/com/fastcampus/ch3/di3/Main.java
+++ boot/ch3/src/main/java/com/fastcampus/ch3/di3/Main.java
@@ -14,7 +14,13 @@ public class Main {
 
         // AC를 생성 - AC의 설정파일은 AppConfig.class 자바설정
         ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        Car car = (Car)ac.getBean("car"); // byName 객체(빈)을 조회
+        System.out.println("car = " + car);
 
+        Car car2 = (Car)ac.getBean("car");
+        Car car3 = (Car)ac.getBean("car");
+        System.out.println("car2 = " + car2);
+        System.out.println("car3 = " + car3);
 
     }
 }
