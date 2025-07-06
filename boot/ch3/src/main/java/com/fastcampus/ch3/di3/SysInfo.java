5698b692f63fc4a4d82901ecfef7c4cf8e01cfbf
이영현 <hong.git@yahoo.com>
2025-07-06 11:04:31 +0900

    Create SysInfo.java

---
+++ boot/ch3/src/main/java/com/fastcampus/ch3/di3/SysInfo.java
@@ -0,0 +1,33 @@
+package com.fastcampus.ch3.di3;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@PropertySource("setting.properties")
+public class SysInfo {
+    @Value("#{systemProperties['user.timezone']}")
+    String timeZone;
+    @Value("#{systemEnvironment['USERNAME']}")
+    String currDir;
+
+    @Value("${autosaveDir}")
+    String autosaveDir;
+
+    @Value("${autosaveInterval}")
+    int autosaveInterval;
+    @Value("${autosave}")
+    boolean autosave;
+
+    @Override
+    public String toString() {
+        return "SysInfo{" +
+                "timeZone='" + timeZone + '\'' +
+                ", currDir='" + currDir + '\'' +
+                ", autosaveDir='" + autosaveDir + '\'' +
+                ", autosaveInternal=" + autosaveInterval +
+                ", autosave=" + autosave +
+                '}';
+    }
+}
