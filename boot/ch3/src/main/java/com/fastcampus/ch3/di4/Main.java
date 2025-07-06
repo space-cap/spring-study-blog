fae244dbf1c3a16de597a0521bf7806b9723e6f1
이영현 <hong.git@yahoo.com>
2025-07-06 19:39:15 +0900

    aa

--- boot/ch3/src/main/java/com/fastcampus/ch3/di4/Main.java
+++ boot/ch3/src/main/java/com/fastcampus/ch3/di4/Main.java
@@ -1,4 +1,19 @@
 package com.fastcampus.ch3.di4;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
+
+@SpringBootApplication
 public class Main {
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(Main.class, args);
+        String[] beanNames = ctx.getBeanDefinitionNames();
+
+        Arrays.sort(beanNames);
+        Arrays.stream(beanNames).forEach(System.out::println);
+
+    }
 }
