8573c32a6c18561d08b541a9e59a1e96c6eff4ec
이영현 <hong.git@yahoo.com>
2025-07-09 15:29:12 +0900

    aa

--- boot/ch4/src/main/java/com/fastcampus/ch4/BoardRepository.java
+++ boot/ch4/src/main/java/com/fastcampus/ch4/BoardRepository.java
@@ -4,6 +4,7 @@ import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -20,6 +21,19 @@ public interface BoardRepository extends CrudRepository<Board, Long> {
 
     @Query(value = "select title, writer from board", nativeQuery = true)
     List<Object[]> findAllNative2();
+
+    // 제목으로 검색
+    List<Board> findByTitleContaining(String title);
+
+    // 작성자로 검색
+    List<Board> findByWriter(String writer);
+
+    // 조회수 기준으로 정렬
+    List<Board> findAllByOrderByViewCntDesc();
+
+    // 제목과 내용에서 키워드 검색
+    @Query("SELECT b FROM Board b WHERE b.title LIKE %:keyword% OR b.content LIKE %:keyword%")
+    List<Board> findByTitleOrContentContaining(@Param("keyword") String keyword);
 
     //int countByTitle(String title);
     //List<Board> findByTitle(String title);
