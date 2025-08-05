31bd20bd563d3d671248a6055e58495c6f68d6f3
이영현 <hong.git@yahoo.com>
2025-08-05 21:48:50 +0900

    dd

--- smile/src/main/java/com/develead/smile/repository/PaymentTransactionRepository.java
+++ smile/src/main/java/com/develead/smile/repository/PaymentTransactionRepository.java
@@ -1,11 +1,18 @@
 package com.develead.smile.repository;
 import com.develead.smile.domain.PaymentTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface PaymentTransactionRepository extends JpaRepository<PaymentTransaction, Integer> {
-    // [신규 추가] 보고서를 위한 쿼리
-    List<PaymentTransaction> findAllByTransactionDateBetween(LocalDateTime start, LocalDateTime end);
+    // [수정] JOIN FETCH를 사용하여 보고서에 필요한 모든 연관 데이터를 즉시 로딩
+    @Query("SELECT pt FROM PaymentTransaction pt " +
+            "JOIN FETCH pt.billing b " +
+            "JOIN FETCH b.medicalRecord mr " +
+            "JOIN FETCH mr.customer " +
+            "WHERE pt.transactionDate BETWEEN :start AND :end")
+    List<PaymentTransaction> findAllByTransactionDateBetween(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
 }
