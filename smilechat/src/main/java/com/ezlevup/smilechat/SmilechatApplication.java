69c300c37f593c131b780684209388a46cab2eb3
steve <hong.git@yahoo.com>
2025-08-11 18:02:35 +0900

    i

---
+++ smilechat/src/main/java/com/ezlevup/smilechat/SmilechatApplication.java
@@ -0,0 +1,13 @@
+package com.ezlevup.smilechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmilechatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmilechatApplication.class, args);
+	}
+
+}
---
+++ smilechat/src/test/java/com/ezlevup/smilechat/SmilechatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezlevup.smilechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmilechatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
